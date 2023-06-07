b8fc5b2683385b2d7b5663db88fc06fe13b30323
Cristobal Garcia <cristobal_garcia@comunidad.unam.mx>
2023-06-06 19:51:07 -0600

    Prueba tecnica

---
+++ WebApplication/WebApplication/Models/DBConn.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+
+public class User
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; }
+    public string Email { get; set; }
+    // Agrega más propiedades según las necesidades de tu entidad Usuario
+}
+public class AppDbContext : DbContext
+{
+    public DbSet<User> Usuarios { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("LogginDB"); // Reemplaza "TuCadenaDeConexion" con tu cadena de conexión real.
+    }
+}
+
---
+++ WebApplication/WebApplication/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+public class Startup
+{
+    private readonly IConfiguration _configuration;
+
+    public Startup(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(_configuration.GetConnectionString("LoggingDB")));
+
+        // Otros servicios y configuraciones...
+    }
+}
